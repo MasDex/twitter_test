441eb8001171bf0e4e1320d347eab8991d4d70a2
masdex <paul.suffel>
2019-04-07 21:38:54 +0200

    Added TwitterClient

--- twitter_app.py
+++ twitter_app.py
@@ -6,6 +6,38 @@ from tweepy import Cursor
 
 import credentials
 
+#### Twitter Client #####
+
+class TwitterClient():
+
+    def __init__(self, twitter_user=None):
+        self.auth = TwitterAuthenticator().authenticate_twitter_app()
+        self.twitter_client = API(self.auth)
+
+        self.twitter_user = twitter_user
+
+
+    def get_user_timeline_tweets(self, num_tweets):
+        tweets = []
+        for tweet in Cursor(self.twitter_client.user_timeline, id = self.twitter_user).items(num_tweets):
+            tweets.append(tweet)
+        return tweets
+
+    def get_friend_list(self, num_friends):
+        friend_list = []
+        for friend in Cursor(self.twitter_client.friends).items(num_friends):
+            friend_list.append(friend)
+        return friend_list
+
+    def get_home_timeline_tweets(self, num_tweets):
+        home_tweets = []
+        for tweet in Cursor(self.twitter_client.home_timeline).items(num_tweets):
+            home_tweets.append(tweet)
+        return home_tweets
+
+
+
+
 #####Twitter Authenticator ###
 class TwitterAuthenticator():
 
@@ -29,8 +61,10 @@ class TwitterStreamer():
         listener = TwitterListener(fetched_tweets_filename)
 
         auth = self.twitter_authenticator.authenticate_twitter_app()
+
         stream = Stream(auth, listener)
         stream.filter(track=hash_tag_list)
+
 
 
 class TwitterListener(StreamListener):
@@ -49,14 +83,20 @@ class TwitterListener(StreamListener):
         return True
 
     def on_error(self, status):
+        if status == 420:
+            #Returning False on_data method in case rate limit occurs.
+            return False
         print(status)
 
 if __name__ == "__main__":
     hash_tag_list = ['Donald Trump']
     fetched_tweets_filename = 'tweets.json'
 
-    twitter_streamer = TwitterStreamer()
-    twitter_streamer.stream_tweets(fetched_tweets_filename,hash_tag_list)
+    #twitter_streamer = TwitterStreamer()
+    #twitter_streamer.stream_tweets(fetched_tweets_filename,hash_tag_list)
+
+    twitter_client = TwitterClient('baloise_ch')
+    print(twitter_client.get_user_timeline_tweets(5))
 
 
 
